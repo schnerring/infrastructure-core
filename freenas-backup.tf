80de63f920b437b22a1c11fb34ec078d2b2c55b2
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-04-19 04:44:51 +0200

    Add FreeNAS backup storage management policy

--- freenas-backup.tf
+++ freenas-backup.tf
@@ -29,3 +29,24 @@ resource "azurerm_storage_container" "freenas_backup_stctn" {
   name                 = "freenas-backup-stctn"
   storage_account_name = azurerm_storage_account.freenas_backup_st.name
 }
+
+resource "azurerm_storage_management_policy" "freenas_backup_st_policy" {
+  storage_account_id = azurerm_storage_account.freenas_backup_st.id
+
+  rule {
+    name    = "rule1"
+    enabled = true
+    filters {
+      blob_types = ["blockBlob"]
+    }
+    actions {
+      base_blob {
+        tier_to_cool_after_days_since_modification_greater_than    = 7
+        tier_to_archive_after_days_since_modification_greater_than = 30
+      }
+      snapshot {
+        delete_after_days_since_creation_greater_than = 30
+      }
+    }
+  }
+}
