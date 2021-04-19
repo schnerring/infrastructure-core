04eec5db96d15b0f3638600a70fc1c34bf9f927d
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-04-19 02:11:03 +0200

    Add empty Terraform files

---
+++ provider.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = "= 0.14.9"
+
+  required_providers {
+    azuread = {
+      source  = "azuread"
+      version = "=1.4.0"
+    }
+
+    azurerm = {
+      source  = "azurerm"
+      version = "=2.55.0"
+    }
+
+    random = {
+      source  = "random"
+      version = "=3.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
