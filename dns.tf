651f192e2eac0c5f48e244bd3b8e3a3ad6f102cc
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-04-25 13:24:30 +0200

    Add ProtonMail DNS records

--- dns.tf
+++ dns.tf
@@ -2,6 +2,74 @@ resource "cloudflare_zone" "schnerring_net_zone" {
   zone = "schnerring.net"
 }
 
+# ProtonMail
+
+resource "cloudflare_record" "protonmail_verification" {
+  zone_id = cloudflare_zone.schnerring_net_zone.id
+  name    = "@"
+  type    = "TXT"
+  value   = "protonmail-verification=15dc53d4ac7f44c8c021a551bf61ed21410beab5"
+  ttl     = 86400
+}
+
+resource "cloudflare_record" "protonmail_mx_1" {
+  zone_id  = cloudflare_zone.schnerring_net_zone.id
+  name     = "@"
+  type     = "MX"
+  value    = "mail.protonmail.ch"
+  ttl      = 86400
+  priority = 10
+}
+
+resource "cloudflare_record" "protonmail_mx_2" {
+  zone_id  = cloudflare_zone.schnerring_net_zone.id
+  name     = "@"
+  type     = "MX"
+  value    = "mailsec.protonmail.ch"
+  ttl      = 86400
+  priority = 20
+}
+
+resource "cloudflare_record" "protonmail_spf" {
+  zone_id = cloudflare_zone.schnerring_net_zone.id
+  name    = "@"
+  type    = "TXT"
+  value   = "v=spf1 include:_spf.protonmail.ch mx ~all"
+  ttl     = 86400
+}
+
+resource "cloudflare_record" "protonmail_dkim_1" {
+  zone_id = cloudflare_zone.schnerring_net_zone.id
+  name    = "protonmail._domainkey"
+  type    = "CNAME"
+  value   = "protonmail.domainkey.dj4kj3y2wss6natk5aychy474cv3uutffovaawtyl2qdey7roqmvq.domains.proton.ch"
+  ttl     = 86400
+}
+
+resource "cloudflare_record" "protonmail_dkim_2" {
+  zone_id = cloudflare_zone.schnerring_net_zone.id
+  name    = "protonmail2._domainkey"
+  type    = "CNAME"
+  value   = "protonmail2.domainkey.dj4kj3y2wss6natk5aychy474cv3uutffovaawtyl2qdey7roqmvq.domains.proton.ch"
+  ttl     = 86400
+}
+
+resource "cloudflare_record" "protonmail_dkim_3" {
+  zone_id = cloudflare_zone.schnerring_net_zone.id
+  name    = "protonmail3._domainkey"
+  type    = "CNAME"
+  value   = "protonmail3.domainkey.dj4kj3y2wss6natk5aychy474cv3uutffovaawtyl2qdey7roqmvq.domains.proton.ch"
+  ttl     = 86400
+}
+
+resource "cloudflare_record" "protonmail_dmarc" {
+  zone_id = cloudflare_zone.schnerring_net_zone.id
+  name    = "_dmarc"
+  type    = "TXT"
+  value   = "v=DMARC1; p=none; rua=mailto:dmarc@schnerring.net"
+  ttl     = 86400
+}
+
 # GitHub Pages
 
 resource "cloudflare_record" "gh_pages_cname_apex" {
