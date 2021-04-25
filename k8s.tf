04291fb5250594b73c112c0886128aeec757f230
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-04-25 02:23:47 +0200

    Add AKS cluster

---
+++ k8s.tf
@@ -0,0 +1,27 @@
+resource "azurerm_resource_group" "k8s_rg" {
+  name     = "k8s-rg"
+  location = var.location
+}
+
+resource "random_id" "random" {
+  byte_length = 2
+}
+
+resource "azurerm_kubernetes_cluster" "k8s_aks" {
+  name                = "k8s-aks"
+  resource_group_name = azurerm_resource_group.k8s_rg.name
+  location            = var.location
+  tags                = var.tags
+
+  dns_prefix = "k8saks${random_id.random.dec}"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
