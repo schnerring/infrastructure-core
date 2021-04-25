f6079cb7267e3fa04ebf8ca6b9dadb6d7d2f45f3
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-04-26 01:00:10 +0200

    Deploy Traefik v2 with Helm

--- k8s.tf
+++ k8s.tf
@@ -21,3 +21,49 @@ resource "azurerm_kubernetes_cluster" "k8s_aks" {
     type = "SystemAssigned"
   }
 }
+
+# cert-manager
+
+resource "kubernetes_namespace" "k8s_cert_manager_ns" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "helm_release" "k8s_cert_manager_helm" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "1.3.1"
+  namespace  = kubernetes_namespace.k8s_cert_manager_ns.metadata[0].name
+}
+
+# Traefik v2
+
+resource "kubernetes_namespace" "traefik_ns" {
+  metadata {
+    name = "traefik"
+  }
+}
+
+resource "helm_release" "traefik_helm" {
+  name       = "traefik"
+  repository = "https://helm.traefik.io/traefik"
+  chart      = "traefik"
+  version    = "9.18.2"
+  namespace  = kubernetes_namespace.traefik_ns.metadata[0].name
+}
+
+data "kubernetes_service" "traefik_svc" {
+  metadata {
+    name      = helm_release.traefik_helm.name
+    namespace = helm_release.traefik_helm.namespace
+  }
+}
+
+resource "cloudflare_record" "traefik" {
+  zone_id = cloudflare_zone.schnerring_net_zone.id
+  name    = "*.traefik"
+  type    = "A"
+  value   = data.kubernetes_service.traefik_svc.status.0.load_balancer.0.ingress.0.ip
+}
