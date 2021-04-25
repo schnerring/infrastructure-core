6d5f51341ab0ed3398cb5e10e0902de9c324cbbc
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-04-25 13:55:05 +0200

    Add Cloudflare zone DNS servers to TF outputs

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,10 @@
+output "dns_name_servers" {
+  value       = cloudflare_zone.schnerring_net_zone.name_servers
+  description = "Cloudflare-assigned name servers."
+}
+
 output "kube_config" {
   value       = azurerm_kubernetes_cluster.k8s_aks.kube_config_raw
-  sensitive   = true
   description = "kubeconfig for kubectl access."
+  sensitive   = true
 }
