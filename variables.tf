5ae695b9b41863558f632850dd68068cdb9c71db
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-04-26 03:29:05 +0200

    Add Let's Encrypt staging ClusterIssuer

--- variables.tf
+++ variables.tf
@@ -13,3 +13,15 @@ variable "tags" {
     "Project"              = "infrastructure"
   }
 }
+
+variable "letsencrypt_email" {
+  type        = string
+  description = "Email address where Let's Encrypt will contact you about expiring certificates and issues related to your account."
+  sensitive   = true
+}
+
+variable "letsencrypt_cloudflare_api_token" {
+  type        = string
+  description = "Cloudflare API token with Zone-DNS-Edit and Zone-Zone-Read permissions, which is required for DNS01 challenge validation."
+  sensitive   = true
+}
