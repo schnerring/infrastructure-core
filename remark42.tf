5862a4300d9f6a8f341810f427d2dbe42f7603de
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-04-28 01:09:34 +0200

    Add Remark42 Deployment

--- remark42.tf
+++ remark42.tf
@@ -23,3 +23,89 @@ resource "kubernetes_persistent_volume_claim" "remark42" {
     }
   }
 }
+
+locals {
+  remark42_image_version = "v1.7.1"
+}
+
+resource "random_password" "remark42_secret" {
+  length = 64
+}
+
+resource "kubernetes_deployment" "remark42" {
+  metadata {
+    name      = "remark42-deploy"
+    namespace = "remark42"
+    labels = {
+      "app" = "remark42"
+    }
+  }
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        "app" = "remark42"
+      }
+    }
+
+    strategy {
+      type = "Recreate"
+    }
+
+    template {
+      metadata {
+        labels = {
+          "app" = "remark42"
+        }
+      }
+
+      spec {
+        hostname       = "remark42"
+        restart_policy = "Always"
+
+        container {
+          name  = "remark42"
+          image = "umputun/remark42:${local.remark42_image_version}"
+
+          volume_mount {
+            mount_path = "/srv/var"
+            name       = "remark42-vol"
+          }
+
+          port {
+            container_port = 8080
+          }
+
+          env {
+            name  = "REMARK_URL"
+            value = "https://remark42.k8s.schnerring.net"
+          }
+
+          env {
+            name  = "SITE"
+            value = "schnerring.net"
+          }
+
+          env {
+            name  = "SECRET"
+            value = random_password.remark42_secret.result
+          }
+
+          env {
+            name  = "AUTH_ANON"
+            value = "true"
+          }
+        }
+
+        volume {
+          name = "remark42-vol"
+
+          persistent_volume_claim {
+            claim_name = "remark42-pvc"
+          }
+        }
+      }
+    }
+  }
+}
