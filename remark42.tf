3a2d99abcf88e7c5b316dc452127043cb51bc3ec
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-04-28 00:51:01 +0200

    Add Remark42 Persistent Volume Claim

--- remark42.tf
+++ remark42.tf
@@ -3,3 +3,23 @@ resource "kubernetes_namespace" "remark42" {
     name = "remark42"
   }
 }
+
+resource "kubernetes_persistent_volume_claim" "remark42" {
+  metadata {
+    name      = "remark42-pvc"
+    namespace = "remark42"
+    labels = {
+      "app" = "remark42-pvc"
+    }
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+
+    resources {
+      requests = {
+        "storage" = "100Mi"
+      }
+    }
+  }
+}
