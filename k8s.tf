f8b3bdb0cf3d87233061130a1125b88e6184ef0a
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-10-19 13:52:40 +0200

    Enable k8s auto-upgrade

--- k8s.tf
+++ k8s.tf
@@ -4,10 +4,11 @@ resource "azurerm_resource_group" "k8s" {
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
-  name                = "k8s-aks"
-  resource_group_name = azurerm_resource_group.k8s.name
-  location            = var.location
-  tags                = var.tags
+  name                      = "k8s-aks"
+  resource_group_name       = azurerm_resource_group.k8s.name
+  location                  = var.location
+  tags                      = var.tags
+  automatic_channel_upgrade = "stable"
 
   dns_prefix = "k8saks${random_id.random.dec}"
 
