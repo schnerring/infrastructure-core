5e078eaac961e735ed28589d6ac3f7f0b572b919
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-02-19 02:04:33 +0100

    Upgrade Traefik

--- k8s.tf
+++ k8s.tf
@@ -103,7 +103,7 @@ resource "helm_release" "traefik" {
   name       = "traefik"
   repository = "https://helm.traefik.io/traefik"
   chart      = "traefik"
-  version    = "9.18.2"
+  version    = "10.14.2"
   namespace  = kubernetes_namespace.traefik.metadata[0].name
 
   set {
