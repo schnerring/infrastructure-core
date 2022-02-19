c1ea3a9365186b1c530786d02fd2654e901492ad
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-02-19 02:08:14 +0100

    Upgrade cert-manager

--- k8s.tf
+++ k8s.tf
@@ -35,7 +35,7 @@ resource "helm_release" "cert_manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
-  version    = "v1.3.1"
+  version    = "v1.7.1"
   namespace  = kubernetes_namespace.cert_manager.metadata[0].name
 
   set {
