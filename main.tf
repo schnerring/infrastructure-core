997342241f8bbd7dfc5252790dcb8cbd393943cb
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-04-20 04:45:25 +0200

    Move provider config to main.tf

--- main.tf
+++ main.tf
@@ -47,24 +47,37 @@ terraform {
       version = "=0.7.2"
     }
   }
-}
 
-terraform {
   backend "azurerm" {}
 }
 
-# Core module
-
-module "core" {
-  source = "./core"
-
-  location = var.location
-  tags     = var.tags
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
 }
 
-# Kubernetes module
+provider "cloudflare" {}
 
-# Postgres module
+provider "helm" {
+  kubernetes {
+    host = azurerm_kubernetes_cluster.k8s.kube_config.0.host
+
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)
+  }
+}
+
+provider "kubernetes" {
+  host = azurerm_kubernetes_cluster.k8s.kube_config.0.host
+
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)
+}
 
 provider "postgresql" {
   host     = "localhost" # kubectl port-forward TODO put into vars?
@@ -74,12 +87,31 @@ provider "postgresql" {
   sslmode  = "disable"
 }
 
+module "core" {
+  source = "./core"
+
+  location = var.location
+  tags     = var.tags
+}
+
 resource "random_password" "matrix_synapse_db" {
   length = 64
 }
 
 resource "random_password" "plausible_db" {
   length = 64
+}
+
+module "kubernetes" {
+  source = "./kubernetes"
+
+  matrix_synapse_db       = var.matrix_synapse_db
+  matrix_synapse_username = var.matrix_synapse_db_username
+  matrix_synapse_password = random_password.matrix_synapse_db.result
+
+  plausible_db       = var.plausible_db
+  plausible_username = var.plausible_db_username
+  plausible_password = random_password.plausible_db.result
 }
 
 module "postgres" {
