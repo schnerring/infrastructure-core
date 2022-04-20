2809f8b58a5cb65c17928411524078e2d49720e4
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-04-20 04:47:39 +0200

    Add new AKS cluster config

---
+++ core/aks.tf
@@ -0,0 +1,32 @@
+resource "azurerm_resource_group" "aks" {
+  name     = "aks-rg"
+  location = "East US"
+}
+
+# Azure Kubernetes Service cluster hosting core web services of my website https://schnerring.net
+#   - PostgreSQL
+#   - Plausible Analytics
+#   - Remark42
+#   - Matrix Synapse
+
+resource "azurerm_kubernetes_cluster" "web_core" {
+  name                = "web-core-aks"
+  resource_group_name = azurerm_resource_group.aks.name
+  location            = azurerm_resource_group.aks.location
+  tags                = var.tags
+
+  automatic_channel_upgrade = "stable"
+  dns_prefix                = "web-core-aks-${random_id.default.hex}"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 1
+    vm_size         = "Standard_D2as_v4"
+    os_disk_type    = "Ephemeral"
+    os_disk_size_gb = 50
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
